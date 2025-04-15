d85f82fe93a2fa1e2250c000426c5ea2966745ad
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-15 12:25:53 -0300

    Update calculof_conexo.rb

--- Ruby/calculof_conexo.rb
+++ Ruby/calculof_conexo.rb
@@ -1,54 +1,59 @@
 require 'rgl/adjacency'
 require 'rgl/traversal'
 
-g = RGL::DirectedAdjacencyGraph.new
+class TarjanSCC
+  def initialize(grafo)
+    @grafo = grafo
+    @visita = 0
+    @visitados = []
+    @indices = {}
+    @ultimos = {}
+    @componentes = []
+  end
 
-# Lê as arestas do arquivo "entrada.txt"
+  def encontrar_componentes
+    @grafo.each_vertex do |no|
+      dfs(no) unless @indices.key?(no)
+    end
+    @componentes
+  end
+
+  private
+
+  def dfs(no)
+    @indices[no] = @visita
+    @ultimos[no] = @visita
+    @visita += 1
+    @visitados << no
+
+    @grafo.adjacent_vertices(no).each do |vizinho|
+      if !@indices.key?(vizinho)
+        dfs(vizinho)
+        @ultimos[no] = [@ultimos[no], @ultimos[vizinho]].min
+      elsif @visitados.include?(vizinho)
+        @ultimos[no] = [@ultimos[no], @indices[vizinho]].min
+      end
+    end
+
+    if @indices[no] == @ultimos[no]
+      componente = []
+      loop do
+        v = @visitados.pop
+        componente << v
+        break if v == no
+      end
+      @componentes << componente
+    end
+  end
+end
+
+g = RGL::DirectedAdjacencyGraph.new
 File.readlines("entrada.txt").each do |linha|
   de, para = linha.strip.split
   g.add_edge(de, para)
 end
 
-@visita = 0
-@visitados = []
-@indices = {}
-@ultimos = {}
-@componentes_fortemente_conexos = []
-
-def funcao(g, no)
-  @indices[no] = @visita
-  @ultimos[no] = @visita
-  @visita += 1
-  @visitados << no
-
-  g.adjacent_vertices(no).each do |vizinho|
-    if !@indices.key?(vizinho)
-      funcao(g, vizinho)
-      @ultimos[no] = [@ultimos[no], @ultimos[vizinho]].min
-    elsif @visitados.include?(vizinho)
-      @ultimos[no] = [@ultimos[no], @indices[vizinho]].min
-    end
-  end
-  
- if @indices[no] == @ultimos[no]
-    componente_atual = []
-    loop do
-      v = @visitados.pop
-      componente_atual << v
-      break if v == no
-    end
-    @componentes_fortemente_conexos << componente_atual
-  end
-end
-g.each_vertex do |no|
-  funcao(g, no) unless @indices.key?(no)
-end
-
-@componentes_fortemente_conexos.each_with_index do |componente, i|
-  puts "Componente f-conexo #{i + 1} sendo:\n#{componente}"
-end
-
-puts "do grafo:"
-g.each_edge do |de, para|
-  puts "#{de} #{para}"
+tarjan = TarjanSCC.new(g)
+tarjan.encontrar_componentes.each_with_index do |comp, i|
+  puts "Componente #{i + 1}: #{comp.join(', ')}"
 end
