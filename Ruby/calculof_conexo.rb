2d8215cc3293affb29203c8259e1abcd6d9625a7
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-15 11:39:39 -0300

    Update calculof_conexo.rb

--- Ruby/calculof_conexo.rb
+++ Ruby/calculof_conexo.rb
@@ -1,1 +1,16 @@
+require 'rgl/adjacency'
+require 'rgl/traversal'
 
+g = RGL::DirectedAdjacencyGraph.new
+
+# Lê as arestas do arquivo "entrada.txt"
+File.readlines("entrada.txt").each do |linha|
+  de, para = linha.strip.split
+  g.add_edge(de, para)
+end
+
+@visita = 0
+@visitados = []
+@indices = {}
+@ultimos = {}
+@componentes_fortemente_conexos = []
